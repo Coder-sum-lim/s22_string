cade8e6dbb5b50e607fd720d9371e40d92cb8e3f
mus-mil <aminsabirov2006@gmail.com>
2024-12-04 19:33:08 +0300

    new files

---
+++ src/s21_string.h
@@ -0,0 +1,10 @@
+#ifndef s21_string
+#define s21_string
+
+#include <stdio.h>
+#include <string.h>
+
+char *s21_strerror(int errnum);
+size_t s21_strlen(const char *str);
+
+#endif
---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include "s21_string.h"
+#include <check.h>
+
+int main(void) {
+    int number_failed;
+    Suite *suite;
+    SRunner *runner;
+
+    runner = srunner_create(suite);
+
+    srunner_run_all(runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(runner);
+    
+    srunner_free(runner);
+    
+    return (number_failed == 0) ? 0 : 1;
+}
