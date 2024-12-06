9066d5b30b13e6b2d169e467ad55fe867aafe321
mus-mil <aminsabirov2006@gmail.com>
2024-12-06 17:55:47 +0300

    makefile

--- src/s21_string.h
+++ src/s21_string.h
@@ -9,15 +9,15 @@
 #include <wchar.h>
 
 
-#define s21_NULL ((void *)0)
+#define S21_NULL ((void *)0)
 typedef unsigned long s21_size_t;
 
+int s21_sscanf(const char *str, const char *format, ...);
+int s21_sprintf(char *str, const char *format, ...);
 void *s21_memchr(const void *str, int c, size_t n);
 int s21_memcmp(const void *str1, const void *str2, size_t n);
 void *s21_memcpy(void *dest, const void *src, size_t n);
 char *s21_strerror(int errnum);
 size_t s21_strlen(const char *str);
-int s21_sscanf(const char *str, const char *format, ...);
-int s21_sprintf(char *str, const char *format, ...);
 
 #endif
